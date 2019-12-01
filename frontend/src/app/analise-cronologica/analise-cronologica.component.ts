4bc0e10be94a7f95010b25561e7aafaf9403ac4f
Ana Julia <anajuliabit@Anas-MacBook-Air.local>
2019-12-01 05:37:43 -0300

    analise-cronologica

--- frontend/src/app/analise-cronologica/analise-cronologica.component.ts
+++ frontend/src/app/analise-cronologica/analise-cronologica.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  ChartDataSets,
+  ChartOptions,
+  ChartLegendLabelOptions,
+  ChartLegendOptions
+} from "chart.js";
+import { Label, Color } from "ng2-charts";
 
 @Component({
   selector: "app-analise-cronologica",
@@ -6,33 +13,71 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./analise-cronologica.component.scss"]
 })
 export class AnaliseCronologicaComponent implements OnInit {
-  lineChartLabels = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-    24
+  public lineChartData: ChartDataSets[] = [
+    {
+      data: [30, 39, 45, 40, 35, 30, 39, 45, 60, 62, 65, 70],
+      label: "Incêndio"
+    },
+    { data: [20, 25, 38, 50, 62, 60, 48, 50, 42, 37, 31, 20], label: "Buraco" },
+    { data: [25, 28, 30, 20, 15, 0], label: "Assalto" }
   ];
+  public lineChartLabels: Label[] = [
+    "6h",
+    "8h",
+    "10h",
+    "12h",
+    "14h",
+    "16h",
+    "18h",
+    "20h",
+    "22h",
+    "00"
+  ];
+
+  public lineChartOptions: ChartOptions & { annotation: any } = {
+    responsive: true,
+    spanGaps: false,
+    annotation: {
+      annotations: [
+        {
+          type: "line",
+          mode: "vertical",
+          scaleID: "x-axis-0",
+          value: "March",
+          borderColor: "orange",
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: "orange",
+            content: "LineAnno"
+          }
+        }
+      ]
+    },
+    legend: { align: "end", labels: { boxWidth: 20, usePointStyle: true } }
+  };
+
+  public lineChartColors: Color[] = [
+    {
+      borderColor: "#DB8DA1",
+      backgroundColor: "#ffffff00"
+    },
+    {
+      borderColor: "#B8D19C",
+      backgroundColor: "#ffffff00"
+    },
+    {
+      borderColor: "#E4B386",
+      backgroundColor: "#ffffff00"
+    }
+  ];
+  public lineChartLegend = true;
+
+  public barChartOptions: any = {
+    legend: { position: "bottom" }
+  };
+  public lineChartType = "line";
+  public lineChartPlugins = [];
 
   constructor() {}
 
